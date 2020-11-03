cf3af607781b28f766f5c314cc1f33ccb0922898
blank <yongchun.long@pitayagames.com>
2020-11-03 17:57:44 +0800

    增加参数支持

--- main.go
+++ main.go
@@ -2,50 +2,24 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
-	"github.com/go-redis/redis/v8"
 	"github.com/lycblank/xlsx2db/db"
 )
 
 func main() {
+	src := flag.String("s", "", "源目录")
+	dst := flag.String("d", "", "目的目录")
+	pkg := flag.String("pkg", "db", "包名")
+	flag.Parse()
+	if *src == "" {
+		panic("源目录不存在")
+	}
+	if *dst == "" {
+		panic("目的目录不存在")
+	}
 	g := db.NewGorm(nil)
-	fmt.Println(g.TransXLSXDir(context.Background(), "sample/data", "sample/go", "db"))
+	fmt.Println(g.TransXLSXDir(context.Background(), *src, *dst, *pkg))
 	return
-	rdb := redis.NewClient(&redis.Options{
-		Addr:     "10.10.104.223:6379",
-		Password: "", // no password set
-		DB:       0,  // use default DB
-	})
-	ctx := context.Background()
-	pipe := rdb.Pipeline()
-
-	cmd1 := pipe.HGet(ctx, "hello", "world")
-	cmd2 := pipe.HGet(ctx, "hello", "world")
-	cmd3 := pipe.HGet(ctx, "hello", "world")
-	cmd4 := pipe.HGet(ctx, "hello", "world")
-	pipe.Process(ctx, cmd1)
-	pipe.Process(ctx, cmd2)
-	pipe.Process(ctx, cmd3)
-	pipe.Process(ctx, cmd4)
-
-
-	cmd5 := pipe.HGet(ctx, "blank", "good")
-	cmd6 := pipe.HGet(ctx, "blank", "good1")
-	cmd7 := pipe.HGet(ctx, "blank", "good2")
-	cmd8 := pipe.HGet(ctx, "blank", "good3")
-	pipe.Process(ctx, cmd5)
-	pipe.Process(ctx, cmd6)
-	pipe.Process(ctx, cmd7)
-	pipe.Process(ctx, cmd8)
-	pipe.Exec(ctx)
-	//fmt.Println(err)
-	fmt.Println(cmd1.String())
-	fmt.Println(cmd2.String())
-	fmt.Println(cmd3.String())
-	fmt.Println(cmd4.String())
-	fmt.Println(cmd5.String())
-	fmt.Println(cmd6.String())
-	fmt.Println(cmd7.String())
-	fmt.Println(cmd8.String())
 }
 
